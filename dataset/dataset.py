b8cc3b6c0ccfff834ddcc3f225e7936f10a9253e
yuanfei-Wang <1025395892@qq.com>
2025-04-18 16:32:23 +0800

    dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -26,7 +26,6 @@ class Episode_Buffer:
         self.action = []
 
     def add(self, pc, env_state, action):
-        #pc, env_state = obs_wrapper(obs)
         self.pcs.append(pc.cpu().numpy())
         self.env_state.append(env_state.cpu().numpy())
         self.action.append(action.cpu().numpy())
@@ -45,8 +44,6 @@ class Experience:
         self.meta = {"episode_ends": []}
     
     def append(self, episode:Episode_Buffer):
-        # new_pcs = torch.tensor(episode.pcs)
-        # downsample, idx = sample_farthest_points(new_pcs, self.sample_pcs_num)
         if len(episode.pcs) == 0:
             # print("skip empty eps")
             return        
@@ -58,17 +55,12 @@ class Experience:
             self.data["pcs"] = np.concatenate([self.data["pcs"], np.array(episode.pcs)])
             self.data["env_state"] = np.concatenate([self.data["env_state"], np.array(episode.env_state)])
             self.data["action"] = np.concatenate([self.data["action"], np.array(episode.action)])
-        # self.data["pcs"] = self.data["pcs"] + episode.pcs
-        # self.data["pose"] = self.data["pose"] + episode.pose
-        # self.data["action"] = self.data["action"] + episode.action
         new_end = self.data["pcs"].shape[0]
-        # print(new_end)
         self.meta["episode_ends"].append(new_end)
 
     def save(self, path):
         # save data and meta in one file as npy
         self.meta["episode_ends"] = np.array(self.meta["episode_ends"])
-        #data_save = {"data": self.data, "meta": self.meta}
         zarr_group = zarr.open(path, 'w')
         nested_dict_save(zarr_group, self.data, 'data')
         nested_dict_save(zarr_group, self.meta, 'meta')
@@ -116,17 +108,6 @@ def sample_sequence(train_data, obs_length, action_length,
             else:
                 data = sample
 
-        # sample = input_arr[buffer_start_idx:buffer_end_idx]
-        # data = sample
-        # if (sample_start_idx > 0) or (sample_end_idx < sequence_length):
-        #     data = np.zeros(
-        #         shape=(sequence_length,) + input_arr.shape[1:],
-        #         dtype=input_arr.dtype)
-        #     if sample_start_idx > 0:
-        #         data[:sample_start_idx] = sample[0]
-        #     if sample_end_idx < sequence_length:
-        #         data[sample_end_idx:] = sample[-1]
-        #     data[sample_start_idx:sample_end_idx] = sample
         result[key] = data
     return result
 
@@ -160,20 +141,12 @@ class ManipDataset(torch.utils.data.Dataset):
             action_data_list.append(data['data']['action'])
             env_state_list.append(data['data']['env_state'])
 
-        # pcs = np.moveaxis(dataset_root['data']['pcs'][:], 1, -1)
         '''
         seg pointcloud don not need moveaxis
         '''
-        # ipdb.set_trace()
         pcs = np.concatenate(pcs_list, axis=0)
         action_data = np.concatenate(action_data_list, axis=0)
         env_state = np.concatenate(env_state_list, axis=0)
-        # max length == 7
-        # action_pos = action_data[:,:8]
-        #env_state_data = np.concatenate([dataset_root['data']['env_state'][:,:25], dataset_root['data']['env_state'][:,26:]], axis=-1)
-        # action_gripper = action_data[:,14:16]/10  # normalize to [-1,1]
-        # print(action_gripper)
-        # action_train = np.concatenate([action_pos, action_gripper], axis=1)
         action_train = action_data
         train_data = {
             'pcs': pcs,
@@ -181,8 +154,6 @@ class ManipDataset(torch.utils.data.Dataset):
             'action': action_train#dataset_root['data']['action'][:]
         }
         episode_ends = np.concatenate(episode_end_list, axis=0)
-        # ipdb.set_trace()
-        # print(episode_ends)
         # compute start and end of each state-action sequence
         # also handles padding
         indices = create_sample_indices(
@@ -193,7 +164,6 @@ class ManipDataset(torch.utils.data.Dataset):
         )
         print("pad after is", pred_horizon)
         self.indices = indices
-        # print(train_data_len)
         self.train_data = train_data
         self.pred_horizon = pred_horizon
         self.action_horizon = action_horizon
@@ -203,15 +173,6 @@ class ManipDataset(torch.utils.data.Dataset):
         return len(self.indices)
 
     def __getitem__(self, idx):
-        # compute which segmentation of data is idx in 
-        # seg = 0
-        # for i in range(len(self.train_data_len)):
-        #     if idx < self.train_data_len[i]:
-        #         seg = i
-        #         break
-        #     else:
-        #         idx -= self.train_data_len[i]
-        #print(seg)
         # get the start/end indices for this datapoint
         action_start_idx, action_end_idx, obs_start_idx, obs_end_idx = self.indices[idx]
         # get nomralized data using these indices
@@ -224,10 +185,6 @@ class ManipDataset(torch.utils.data.Dataset):
             obs_start_idx=obs_start_idx,
             obs_end_idx=obs_end_idx
         )
-
-        # discard unused observations
-        # nsample['pcs'] = nsample['pcs'][:self.obs_horizon,:]
-        # nsample['pose'] = nsample['pose'][:self.obs_horizon,:]
         return nsample
 
 def merge_dataset(path_list, to_path):
@@ -246,7 +203,6 @@ def merge_dataset(path_list, to_path):
             new_meta = dataset_root['meta']['episode_ends'][:] + cur_len
             cur_len += dataset_root['meta']['episode_ends'][-1]
             meta = np.concatenate([meta, new_meta])
-            #print(new_meta)
         print(pcs_data.shape, pose_data.shape, action_data.shape, meta.shape)
 
     # save data and meta in one file using zarr
@@ -255,32 +211,3 @@ def merge_dataset(path_list, to_path):
     zarr_group = zarr.open(to_path, 'w')
     nested_dict_save(zarr_group, all_data, 'data')
     nested_dict_save(zarr_group, all_meta, 'meta')
-
-
-if __name__ == "__main__":
-    # merge dataset
-    # path_prefix = "./demo_data/ada/nohis/"
-    # to_path = "./demo_data/ada/nohis/demo_mix.zip"
-    # obj_id = ["005", "012", "013", "022", "031", "065"]
-    # # append obj_id to prefix to get each file path
-    # path_list = [path_prefix + id + "/demo_buffer.zip" for id in obj_id]
-    to_path = "./demo_data/succ/51213223154full.zip"
-    path_list = []
-    path_list.append("./demo_data/succ/512133165/demo_buffer.zip")  # another half is success
-    path_list.append("./demo_data/succ/022_100/demo_buffer.zip") 
-    merge_dataset(path_list,to_path)
-
-    # test dataset
-    # dataset_path = "./demo_data/ada/01-12-01-07-38/demo_buffer.zip"
-    # dataset_root = zarr.open(dataset_path, 'r')
-    #dataset = ManipDataset(dataset_path, 16, 2, 8)
-    # eps_end = np.array(dataset_root['meta']['episode_ends'][:])
-    # print(eps_end)
-    #print(dataset_root['data']['pose'][0,25:])
-    # points = torch.tensor(dataset_root['data']['pcs'][0]).unsqueeze(0)
-    # pcs = Pointclouds(points=points)
-    # fig = plot_scene({"plot1":{"pcs":pcs}})
-    # fig.show()
-    # episode_end = np.array([20])
-    # idxs = create_sample_indices(episode_end, 2, 16, 8)
-    # print(idxs)
